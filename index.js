01fc2d9d669aaa2ef1fb4f723fc3e7d8522bbc95
salsyabila 72 <salsyabila998@gmail.com>
2019-09-26 07:21:02 +0700

    LatihanExpress

--- index.js
+++ index.js
@@ -4,6 +4,23 @@ const port = 8000;
 
 app.get('/', (req, res) => {
     res.send('Halo aku express!');
+});
+
+app.get('/Login', (req, res) => {
+    res.send('Login!');
+});
+
+app.post('/register', (req, res) => {
+    res.send('Request POST for register!');
+});
+
+app.put('/update', (req, res) => {
+    res.send('Hai ini put!');
+});
+
+app.delete('/delete', (req, res) => {
+    res.send('request delete');
 })
+
 
 app.listen(port, ()=> console.log(`App listen on port ${port}`))
